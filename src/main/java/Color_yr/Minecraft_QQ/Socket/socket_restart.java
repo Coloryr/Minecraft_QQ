96d0c66ec9b16febff9df38ad5c1c71c9b01ba3a
Color_yr <402067010@qq.com>
2019-10-09 20:59:52 +0800

    修复bug

--- src/main/java/Color_yr/Minecraft_QQ/Socket/socket_restart.java
+++ src/main/java/Color_yr/Minecraft_QQ/Socket/socket_restart.java
@@ -6,14 +6,13 @@ import Color_yr.Minecraft_QQ.Log.logs;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class socket_restart {
     static boolean is_restart = false;
 
     public boolean socket_restart_start() {
             config.ilog.Log_System("§d[Minecraft_QQ]§5正在连接酷Q");
-        if (logs.Socket_log == true) {
+        if (logs.Socket_log) {
             logs logs = new logs();
             logs.log_write("[socket]正在连接酷Q");
         }
@@ -22,34 +21,19 @@ public class socket_restart {
 
             socket.hand.socket_runFlag = true;
                 config.ilog.Log_System("§d[Minecraft_QQ]§5酷Q已连接");
-            if (logs.Socket_log == true) {
+            if (logs.Socket_log) {
                 logs logs = new logs();
                 logs.log_write("[socket]酷Q已连接");
             }
             is_restart = false;
             socket.hand.socket_first = false;
-            return true;
-        } catch (UnknownHostException e) {
+        } catch (Exception e) {
                 config.ilog.Log_System("§d[Minecraft_QQ]§c酷Q连接失败");
-            if (logs.Socket_log == true) {
+            if (logs.Socket_log) {
                 logs logs = new logs();
                 logs.log_write("[socket]酷Q连接失败");
             }
-            if (logs.Error_log == true) {
-                logs logs = new logs();
-                logs.log_write("[ERROR]" + e.getMessage());
-            }
-            is_restart = false;
-            socket.hand.socket_runFlag = false;
-            socket.hand.socket_first = false;
-            return false;
-        } catch (IOException e) {
-                config.ilog.Log_System("§d[Minecraft_QQ]§c酷Q连接失败");
-            if (logs.Socket_log == true) {
-                logs logs = new logs();
-                logs.log_write("[socket]酷Q连接失败");
-            }
-            if (logs.Error_log == true) {
+            if (logs.Error_log) {
                 logs logs = new logs();
                 logs.log_write("[ERROR]" + e.getMessage());
             }
@@ -58,13 +42,14 @@ public class socket_restart {
             socket.hand.socket_first = false;
             return false;
         }
+        return true;
     }
 
-    public boolean restart_socket() {
-        if (is_restart != true) {
+    public void restart_socket() {
+        if (!is_restart) {
             socket.hand.socket_first = true;
             try {
-                if(!socket.hand.socket.isClosed())
+                if(socket.hand.socket!=null && !socket.hand.socket.isClosed())
                 socket.hand.socket.close();
             } catch (IOException e) {
                 if (logs.Error_log) {
@@ -74,14 +59,13 @@ public class socket_restart {
             }
             socket.hand.readThread = new socket();
             socket.hand.readThread.start();
-            return socket_restart_start();
+            socket_restart_start();
         } else {
                 config.ilog.Log_System("§d[Minecraft_QQ]§c酷Q连接失败");
-            if (logs.Socket_log == true) {
+            if (logs.Socket_log) {
                 logs logs = new logs();
                 logs.log_write("[socket]酷Q连接失败");
             }
-            return false;
         }
     }
 }
